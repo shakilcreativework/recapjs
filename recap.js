457dcd5587f1f5a90188dde164d688dfecb327e8
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-26 10:25:45 +0600

    add some learning info

--- recap.js
+++ recap.js
@@ -5,4 +5,20 @@
     2-2: getElemenstByClassName
     2-3: querySelector
     2-4: querySelectorAll
+
+3. dynamically add element to the dom: document.createElement
+    3-5: set value to the innerHTML
+
+4. get value from a dom element
+    4-1: use innerText (h1, p, h5, div, span, strong)...
+    4-2: use value (input, textArea)
+
+5. add event listen (event handler)
+    5-1: click, focus, blur, onchange, keyup
+    5-2: addEventListener, onclick="myHandler"
+
+6. use function to reduce duplicate code
+
+7. event bubble and event delegate
 */ 
+
