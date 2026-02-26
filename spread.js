7d5c93a6f2e3f4df613019bc1be62e0fd1215634
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-26 12:13:51 +0600

    spread operator used on array or object...

--- spread.js
+++ spread.js
@@ -24,9 +24,17 @@ const result = sum(...params);
 // console.log(arr1);
 // console.log(arr2);
 
-const arr1 = [1,2,3];
-const arr2 = [...arr1];
+// const arr1 = [1,2,3];
+// const arr2 = [...arr1];
 
-arr2.push(4);
-console.log(arr1);
-console.log(arr2);
+// arr2.push(4);
+// console.log(arr1);
+// console.log(arr2);
+
+
+const person = {name: "Alice", age: 30};
+// const person2 = person;
+const person2 = {...person, designation: 'Developer'};
+person.salary = 36000;
+console.log(person);
+console.log(person2);
