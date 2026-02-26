ddff77dcde2cbce5ad71c72c642a75d474b66bc1
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-26 12:07:21 +0600

    spread operator is a most powerfull beacause it can make new arrays element copy and make full new array without reference

--- spread.js
+++ spread.js
@@ -6,3 +6,27 @@ const max2 = Math.max(...numbers);
 console.log(max2);
 console.log(Array.isArray(numbers));
 console.log(...numbers);
+
+
+const params = [45, 12, 3];
+function sum(x, y, z){
+    return x + y + z;
+}
+
+// const result = sum(params);
+const result = sum(...params);
+// console.log(result);
+
+// const arr1 = [1,2,3];
+// const arr2 = arr1;
+
+// arr2.push(4);
+// console.log(arr1);
+// console.log(arr2);
+
+const arr1 = [1,2,3];
+const arr2 = [...arr1];
+
+arr2.push(4);
+console.log(arr1);
+console.log(arr2);
