5fee2fad64e96759bf1848e997dbe5391effb938
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-26 11:01:16 +0600

    amazing, learning of var, let and const between diference

--- let-const.js
+++ let-const.js
@@ -1,5 +1,20 @@
+// console.log(friend);
 const name = 'Bangladesh';
 // name = 'Bangladesh';
 const countryName = 'BD_' + name;
 
-console.log(countryName);
+// console.log(countryName);
+
+let price = 120;
+price = 150;
+// console.log(price);
+
+var friend = 'Salmansha';
+
+
+const dress = ['Shirt', 'Pant', 'Shoe'];
+dress.push('Belt');
+dress.pop('Shoe');
+dress.shift('Belt');
+dress.unshift('Shoe');
+console.log(dress);
