6dcc14157ad98d62bc56e2875f1d84022b2d7c09
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-26 14:10:57 +0600

    amazing, was learning ES6

---
+++ dot-bracket.js
@@ -0,0 +1,9 @@
+const employee = {
+    name: 'John Doe',
+    1: 'Desk one',
+    position: 'Softwore Engineer'
+};
+
+// dot notation
+console.log(employee.name);
+console.log(employee[1]);
---
+++ loop.js
@@ -0,0 +1,19 @@
+const numbers = [1, 2, 3, 4, 5];
+for(const number of numbers){
+    // console.log(number);
+}
+
+// numbers.map(num => {
+//     console.log(num);
+// })
+
+const employee = {
+    name: 'Mufasa',
+    age: 55,
+    kingdom: 'Pride Lands',
+    position: 'Softwore Engineer'
+}
+
+for(const emp in employee){
+    console.log(employee[emp]);
+}
