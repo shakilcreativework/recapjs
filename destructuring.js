4d97778525ac89c584809d46ca537c983deea12a
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-02-26 12:32:23 +0600

    Learnign destructuring can use on array  == array and object == object

---
+++ destructuring.js
@@ -0,0 +1,16 @@
+// const product = {
+//     name: 'IPhone',
+//     price: 799,
+//     description: 'A smartphone by Apple'
+// };
+
+// const newPrice = product.price + 100;
+// const phoneName = product.name;
+
+// console.log(product);
+// console.log(newPrice);
+// console.log(phoneName);
+
+// const product = {name: 'iPhone', price: 799, brand: 'Apple'};
+const {name, price, brand} = {name: 'iPhone', price: 799, brand: 'Apple'};
+console.log(name, price, brand);
